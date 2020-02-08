0765cd0ec95124b289c69f1e5851bd962cb51f7e
Sowmya Mannem <sowmyamannemr@gmail.com>
2020-02-08 12:11:00 +0100

    removed comments in database file

--- src/backend/database.js
+++ src/backend/database.js
@@ -1,14 +1,3 @@
-// const mysql = require("mysql");
-// const dotenv = require("dotenv");
-// dotenv.config(); 
-
-// const { MYSQL_URL } = process.env;
-// const connection = mysql.createPool(MYSQL_URL);
-
-
-// module.exports = connection;
-
-
 const mysql = require("mysql");
 
 require("dotenv").config();
